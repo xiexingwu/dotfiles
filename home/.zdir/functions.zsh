fc8e508337ec9745030a2c0c9ccedd5afb46fb57
Michael Xie <xie.michael.x@gmail.com>
2025-05-17 02:01:59 +1000

    zsh: ls no longer aliased

--- home/.zdir/functions.zsh
+++ home/.zdir/functions.zsh
@@ -1,13 +1,5 @@
-# List directory contents
-function ls(){
-    if (command eza &> /dev/null); then
-        eza $*
-    else
-        ls
-    fi
-}
 function l(){
-    ls -l --group-directories-first -h --git $*
+    eza -l --group-directories-first -h --git $*
 }
 alias ll=l
 function la(){
