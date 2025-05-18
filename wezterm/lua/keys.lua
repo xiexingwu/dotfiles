5a26208854738220e306d07794f711053326c272
Michael Xie <xie.michael.x@gmail.com>
2025-05-18 21:47:34 +1000

    update keymaps: change workspace & improved quitting

--- wezterm/lua/keys.lua
+++ wezterm/lua/keys.lua
@@ -14,32 +14,61 @@ function utils.contains(a, b)
   return false
 end
 
+function change_workspace(win, pane)
+  local workspace = win:active_workspace()
+  local workspaces = wezterm.mux.get_workspace_names()
+  local choices = {}
+  for _, ws in ipairs(workspaces) do
+    if ws:sub(1, 1) ~= "_" then
+      local is_active = ws == workspace and " *" or ""
+      local symbol = " "
+      if ws == "pers" then
+        symbol = "󰋞 "
+      elseif ws == "work" then
+        symbol = " "
+      end
+      table.insert(choices, { label = symbol .. ws .. is_active, id = ws })
+    end
+  end
+  win:perform_action(
+    act.InputSelector {
+      action = wezterm.action_callback(function(win, pane, id, label)
+        if not id and not label then
+          return
+        end
+        win:mux_window():set_workspace(id)
+      end),
+      title = "Workspace",
+      choices = choices,
+      alphabet = '123457890',
+    }, pane
+  )
+end
+
 ---Detach the surface it is the last one. Otherwise close.
 ---@param win any
 ---@param pane any
 ---@param surface "tab"|"pane" what the function is trying to close
 function detach_if_last_else_close(win, pane, surface)
   local isLastTab = #win:mux_window():tabs() == 1
-  local tab = win:active_tab()
-  local isLastPane = #tab:panes() == 1
+  local isLastPane = #pane:tab():panes() == 1
   if surface == "tab" then
     if isLastTab then
       win:perform_action(act.DetachDomain "CurrentPaneDomain", pane)
     else
-      win:perform_action(act.CloseCurrentTab({ confirm = true }), pane)
+      win:perform_action(act.CloseCurrentTab { confirm = true }, pane)
     end
-    return
   elseif surface == "pane" then
     if isLastTab and isLastPane then
       win:perform_action(act.DetachDomain "CurrentPaneDomain", pane)
     else
-      win:perform_action(act.CloseCurrentPane({ confirm = true }), pane)
+      win:perform_action(act.CloseCurrentPane { confirm = true }, pane)
     end
-    return
+  else
+    -- fallback is close current tab
+    wezterm.log_info("Incorrectly called detach_if_last_else_close with " .. surface)
+    win:perform_action(act.CloseCurrentTab { confirm = true }, pane)
   end
-
-  -- fallback is close current tab
-  win:perform_action(act.CloseCurrentTab({ confirm = true }), pane)
 end
 
 function module.apply_to_config(config)
@@ -62,7 +91,8 @@ function module.apply_to_config(config)
     },
     { mods = "CMD",    key = "v",      action = act.PasteFrom("Clipboard") },
 
-    { mods = "LEADER", key = "p",      action = act.ActivateCommandPalette },
+    { mods = "LEADER", key = "p",      action = wezterm.action_callback(change_workspace), },
+    { mods = "LEADER", key = "P",      action = act.ActivateCommandPalette },
     { mods = "LEADER", key = "E",      action = act.CharSelect }, -- [E]moji
 
     { mods = "CMD",    key = "k",      action = act.ClearScrollback("ScrollbackAndViewport") },
@@ -73,6 +103,8 @@ function module.apply_to_config(config)
     { mods = "CMD",    key = "f",      action = act.Search({ CaseInSensitiveString = '' }) },
     { mods = "LEADER", key = "a",      action = act.AttachDomain "unix", },
     { mods = "LEADER", key = "d",      action = act.DetachDomain "CurrentPaneDomain", },
+
+    -- Open/Close new Tabs/Panes
     {
       mods = "CMD",
       key = "q",
@@ -82,13 +114,7 @@ function module.apply_to_config(config)
         cancel = wezterm.action_callback(function() end),
       },
     },
-    {
-      mods = "CMD",
-      key = "w",
-      action = wezterm.action_callback(function(win, pane)
-        detach_if_last_else_close(win, pane, "tab")
-      end)
-    },
+    { mods = "LEADER", key = "q", action = act.QuitApplication, },
     {
       mods = "OPT",
       key = "w",
@@ -96,45 +122,64 @@ function module.apply_to_config(config)
         detach_if_last_else_close(win, pane, "pane")
       end)
     },
+    {
+      mods = "CMD",
+      key = "w",
+      action = wezterm.action_callback(function(win, pane)
+        detach_if_last_else_close(win, pane, "tab")
+      end)
+    },
+    { mods = "CMD|SHIFT",  key = "w",   action = act.DetachDomain "CurrentPaneDomain" },
 
-    { mods = "CMD",        key = "n",          action = act.SpawnWindow },
-    { mods = "CMD",        key = "t",          action = act.SpawnTab("CurrentPaneDomain") },
+    { mods = "CMD",        key = "n",   action = act.SpawnWindow },
+    { mods = "CMD",        key = "t",   action = act.SpawnTab("CurrentPaneDomain") },
 
-    { mods = "CTRL",       key = "Tab",        action = act.ActivateTabRelative(1) },
-    { mods = "CTRL|SHIFT", key = "Tab",        action = act.ActivateTabRelative(-1) },
+    { mods = "CTRL",       key = "Tab", action = act.ActivateTabRelative(1) },
+    { mods = "CTRL|SHIFT", key = "Tab", action = act.ActivateTabRelative(-1) },
+    { mods = "CMD",        key = "1",   action = act.ActivateTab(1) },
 
-    { mods = "LEADER",     key = "v",          action = act.SplitHorizontal({ domain = "CurrentPaneDomain" }) },
-    { mods = "LEADER",     key = "s",          action = act.SplitVertical({ domain = "CurrentPaneDomain" }) },
+    { mods = "LEADER",     key = "v",   action = act.SplitHorizontal({ domain = "CurrentPaneDomain" }) },
+    { mods = "LEADER",     key = "s",   action = act.SplitVertical({ domain = "CurrentPaneDomain" }) },
+    {
+      mods = "LEADER",
+      key = "l",
+      action = wezterm.action_callback(function(win, pane)
+        pane:move_to_new_tab()
+      end)
+    },
+    {
+      mods = "LEADER",
+      key = "L",
+      action = wezterm.action_callback(function(win, pane)
+        pane:move_to_new_window(win:active_workspace())
+      end)
+    },
 
     -- Resize/activate panes (vanilla)
-    { mods = "LEADER",     key = "r",          action = act.ActivateKeyTable({ name = "resize_pane", one_shot = false }) },
-    { mods = "CTRL",       key = "Enter",      action = act.TogglePaneZoomState },
-
-    -- Switch between work/personal workspaces
-    -- { mods = "LEADER",     key = "1",          action = action.SwitchToWorkspace({ name = "work" }) },
-    -- { mods = "LEADER",     key = "2",          action = action.SwitchToWorkspace({ name = "personal" }) },
+    { mods = "LEADER", key = "r",          action = act.ActivateKeyTable({ name = "resize_pane", one_shot = false }) },
+    { mods = "CTRL",   key = "Enter",      action = act.TogglePaneZoomState },
 
     -- Disable minimise & hide
-    { mods = "CMD",        key = "m",          action = act.Nop },
-    { mods = "CMD",        key = "h",          action = act.Nop },
+    { mods = "CMD",    key = "m",          action = act.Nop },
+    { mods = "CMD",    key = "h",          action = act.Nop },
 
     -- Jump word
-    { mods = "OPT",        key = "LeftArrow",  action = act.SendKey({ key = "b", mods = "ALT", }) },
-    { mods = "OPT",        key = "RightArrow", action = act.SendKey({ key = "f", mods = "ALT" }) },
+    { mods = "OPT",    key = "LeftArrow",  action = act.SendKey({ key = "b", mods = "ALT", }) },
+    { mods = "OPT",    key = "RightArrow", action = act.SendKey({ key = "f", mods = "ALT" }) },
     -- { mods = "OPT",        key = "LeftArrow",  action = action.SendKey { mods = "CTRL", key = "LeftArrow" }, },
     -- { mods = "OPT",        key = "RightArrow", action = action.SendKey { mods = "CTRL", key = "RightArrow" }, },
-    { mods = "CMD",        key = "LeftArrow",  action = act.SendString "\x01", }, -- Home
-    { mods = "CMD",        key = "RightArrow", action = act.SendString "\x05", }, -- End
+    { mods = "CMD",    key = "LeftArrow",  action = act.SendString "\x01", }, -- Home
+    { mods = "CMD",    key = "RightArrow", action = act.SendString "\x05", }, -- End
 
 
     -- Font size
-    { mods = "CMD",        key = "=",          action = act.IncreaseFontSize },
-    { mods = "CMD",        key = "-",          action = act.DecreaseFontSize },
-    { mods = "CMD",        key = "0",          action = act.ResetFontSize },
+    { mods = "CMD",    key = "=",          action = act.IncreaseFontSize },
+    { mods = "CMD",    key = "-",          action = act.DecreaseFontSize },
+    { mods = "CMD",    key = "0",          action = act.ResetFontSize },
 
     -- Scroll
-    { mods = nil,          key = "PageUp",     action = act.ScrollByPage(-0.5) },
-    { mods = nil,          key = "PageDown",   action = act.ScrollByPage(0.5) },
+    { mods = nil,      key = "PageUp",     action = act.ScrollByPage(-0.5) },
+    { mods = nil,      key = "PageDown",   action = act.ScrollByPage(0.5) },
 
     {
       mods = 'CMD|SHIFT',
@@ -288,24 +333,9 @@ function module.apply_to_config(config)
     -- local isAltPane = pane:is_alt_screen_active() -- doesn't work when muxing
     -- wezterm.log_info("isAltPane?", isAltPane)
     local title = pane:get_title()
-    local isNvim = string.find(title, "vim")
+    local isNvim = title:find("vim")
     return isNvim
   end
-
-  -- -- Do wezterm/nvim integration
-  -- ---@param dir "Right" | "Left" | "Up" | "Down"
-  -- function activate_pane(dir)
-  --   return wezterm.action_callback(function(window, pane)
-  --     local key = pane_nav[dir]
-  --     if isNvim(pane) then
-  --       wezterm.log_info('passing through keys:' .. pane_nav_mods .. key)
-  --       window:perform_action(act.SendKey({ key = key, mods = pane_nav_mods }), pane)
-  --     else
-  --       wezterm.log_info('changing dir:' .. key)
-  --       window:perform_action(act.ActivatePaneDirection(dir), pane)
-  --     end
-  --   end)
-  -- end
 
   pane_nav = {
     h = "Left",
@@ -317,16 +347,11 @@ function module.apply_to_config(config)
   pane_nav_mods = "CTRL"
   for key, dir in pairs(pane_nav) do
     local event_name = "x-smartsplit-" .. key
-    wezterm.log_info('Defining activate_pane for key:', pane_nav_mods, dir, pane_nav[dir])
-    -- table.insert(config.keys, { key = key, mods = pane_nav_mods, action = activate_pane(dir) })
     table.insert(config.keys, { key = key, mods = pane_nav_mods, action = act.EmitEvent(event_name) })
-    -- table.insert(config.keys, { key = key, mods = pane_nav_mods, action = act.ActivatePaneDirection(dir) })
     wezterm.on(event_name, function(window, pane)
       if isNvim(pane) then
-        wezterm.log_info('passing through keys:' .. pane_nav_mods .. key)
         window:perform_action(act.SendKey({ key = key, mods = pane_nav_mods }), pane)
       else
-        wezterm.log_info('changing direction:' .. dir)
         window:perform_action(act.ActivatePaneDirection(dir), pane)
       end
     end)
